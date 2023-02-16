c2cdc349d9b7c0c8019d96d732a2db0f84542dde
IanTaiAhn <iantaiahn@yahoo.com>
2023-02-15 23:05:03 -0700

    Created dynamic url routing

--- hellodjango/food/urls.py
+++ hellodjango/food/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # root is /food/
     path('', views.index, name="index"),
     path('item', views.item, name="item"),
+    # The name parameter is how we can access this path dynamically
     path('<int:item_id>', views.detail, name="detail"),
     # path('test', views.test, name="test"),
 ]
