7e9729699331cd4a5f05b910cd3eee30270aedc7
IanTaiAhn <iantaiahn@yahoo.com>
2023-02-22 09:26:45 -0700

    Created a new app for users, and added Django's pre-made forms that authenticates users.

--- hellodjango/food/views.py
+++ hellodjango/food/views.py
@@ -5,8 +5,6 @@ from django.template import loader
 from .forms import ItemForm
 # Create your views here.
 
-# so views.index does the thing, so the name of the function determines.
-
 
 def index(request):
     item_list = Item.objects.all()
@@ -18,8 +16,6 @@ def index(request):
     return render(request, 'food/index.html', context)
 #   return HttpResponse(template.render(context, request))
 # They both do the same thing but render syntax is cleaner.
-
-# TODO Delete
 
 
 def item(request):
@@ -57,23 +53,20 @@ def update_item(request, id):
     # What is the difference between pk or id?
     item = Item.objects.get(pk=id)
     form = ItemForm(request.POST or None, instance=item)
-
     if form.is_valid():
         form.save()
         return redirect('food:index')
-
     return render(request, 'food/item-form.html', {'form': form, 'item': item})
 
 
 def delete_item(request, id):
-    # We get the item that the request id is equal to in our ddatabase.
-    # What is the difference between pk or id?
     item = Item.objects.get(pk=id)
-
     # Upon the submitting of our form this code runs.
     if request.method == 'POST':
         item.delete()
         return redirect('food:index')
+# We can access stuff from our database via HTML using Django tags because of the render method.
+# 'food/item-delete.html' and 'item: item' links these things together.
 # We have access to the item variable because of the render path.
 # So in our html for item-delete we can use tags to access info from our database.
     return render(request, 'food/item-delete.html', {'item': item})
