79583b6006e008f1f3f9c5460470dd6c0a349d12
IanTaiAhn <iantaiahn@yahoo.com>
2023-02-22 10:09:48 -0700

    Added an additional field to our registration form.

--- hellodjango/users/views.py
+++ hellodjango/users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegistrationForm
 
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -15,7 +15,7 @@ def register(request):
                 request, f'Welcome {username}! Your account is created')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
 
     # We want to check our request data, and if it is post and valid then we do stuff.
     return render(request, 'users/register.html', {'form': form})
