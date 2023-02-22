7768c0e0c8977eaa5d1a7fc6114489d9014a61ce
IanTaiAhn <iantaiahn@yahoo.com>
2023-02-22 09:57:13 -0700

    Added success message upon user creation.

--- hellodjango/users/views.py
+++ hellodjango/users/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Welcome {username}! Your account is created')
+            return redirect('food:index')
+    else:
+        form = UserCreationForm()
+
+    # We want to check our request data, and if it is post and valid then we do stuff.
     return render(request, 'users/register.html', {'form': form})
